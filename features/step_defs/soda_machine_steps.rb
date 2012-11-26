e14372e9d7d61f3550a3357306053b337a9d8812
Rich Dammkoehler <rpd@noradltd.com>
2012-11-26 10:46:50 -0600

    Rich D: added prescribed steps, and ignore ~ files

---
+++ features/step_defs/soda_machine_steps.rb
@@ -0,0 +1,69 @@
+require 'gherkin'
+
+Given /^an empty soda machine$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fill the machine$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the machine is full$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a partially full soda machine$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the extra soda is not used$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a full soda machine$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a soda machine$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^it has soda$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I use exact change$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I get a soda$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I put in too much money$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I get correct change$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I put in too little money$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I don't get a soda$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I'm told how much more money I need to add$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^it doesn't have soda$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I get my money back$/ do
+  pending # express the regexp above with the code you wish you had
+end
